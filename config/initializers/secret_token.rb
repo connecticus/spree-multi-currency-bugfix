ff0bcfc7d18fb19e283a18840c9bdfec6d4671d6
k@connecticus.org <Ekaterina Belinska>
2014-08-22 15:22:06 +0400

    init spree app w/ pronix/spree-multi-currency

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spree215::Application.config.secret_key_base = '48e15dbb065d812f5557fefe1bfc3ed3aa84738ba9f80cf606c144f50472d3dc290acee6e51b57776c5b356921b97a0d5b9579d0b44d272b096a7d58c75ea32d'
